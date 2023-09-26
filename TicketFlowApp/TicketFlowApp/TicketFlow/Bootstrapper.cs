79db87040036978a4650c4a5d9ebdad35cb16125
dev global <georg.schweizer@omikron.net>
2023-09-26 09:31:03 +0200

    added project

---
+++ TicketFlowApp/TicketFlowApp/TicketFlow/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TicketFlow.ViewModels;
+using TicketFlow.Views;
+
+namespace TicketFlow
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<MainViewModel>();
+        }
+
+    }
+}
---
+++ TicketFlowApp/TicketFlowApp/TicketFlow/ViewModels/MainViewModel.cs
@@ -0,0 +1,69 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketFlow.Views;
+
+namespace TicketFlow.ViewModels
+{
+    public class MainViewModel : Conductor<object>
+    {
+        //private LoginViewModel _loginVM;
+
+        //public MainViewModel(LoginViewModel loginVM)
+        //{
+        //    _loginVM = loginVM;
+        //    ActivateItemAsync(loginVM);
+        //}
+
+        // dynamic title
+        private string activeItemTitle;
+        public string ActiveItemTitle
+        {
+            get { return activeItemTitle; }
+            set
+            {
+                activeItemTitle = value;
+                NotifyOfPropertyChange(() => ActiveItemTitle);
+            }
+        }
+
+        //load child elements >
+        public void LoadDashboard()
+        {
+            ActivateItemAsync(new DashboardViewModel());
+            ActiveItemTitle = "Dashboard";
+
+        }
+
+        public void LoadtableView()
+        {
+            ActivateItemAsync(new TableViewModel());
+            ActiveItemTitle = "Table View";
+        }
+
+        public void LoadAPISettings()
+        {
+            ActivateItemAsync(new APISettingsViewModel());
+            ActiveItemTitle = "API Settings";
+        }
+
+        public void LoadAbout()
+        {
+            ActiveItemTitle = "About";
+            ActivateItemAsync(new AboutViewModel());
+
+        }
+
+        public void LoadLogin()
+        {
+            ActiveItemTitle = "Login";
+            
+            ActivateItemAsync(new LoginViewModel());
+        }
+    }
+
+
+}
